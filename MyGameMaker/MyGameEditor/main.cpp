a014ee7f98726a23cb4de573a7b7db7a7f854b15
Samu163 <125267861+Samu163@users.noreply.github.com>
2024-10-28 18:25:49 +0100

    drag and drop implemented

--- MyGameMaker/MyGameEditor/main.cpp
+++ MyGameMaker/MyGameEditor/main.cpp
@@ -1,211 +1,191 @@
 #include <iostream>
-#include <array>
-#include <vector>
-#include <glm/glm.hpp>
+#include <string>
 #include <IL/il.h>
 #include <IL/ilu.h>
 #include <GL/glew.h>
-#include <GL/freeglut.h>
-#include "Assimp/cimport.h" 
-#include "Assimp/scene.h" 
-#include "Assimp/postprocess.h"
-
-
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <chrono>
+#include <thread>
 #include "MyGameEngine/Camera.h"
 #include "MyGameEngine/Mesh.h"
 #include "MyGameEngine/GameObject.h"
+#include "MyWindow.h"
 
-#include <IL/ilut.h>
 using namespace std;
+using hrclock = chrono::high_resolution_clock;
+using vec3 = glm::dvec3;
 
-
-
+static const glm::ivec2 WINDOW_SIZE(1280, 720);
+static const unsigned int FPS = 60;
+static const auto FRAME_DT = 1.0s / FPS;
 
 static Camera camera;
-static GameObject loadedMesh;
-
-//A bool to get all the keys if pressed
-bool keyStates[256] = { false };
+static GameObject go;
+SDL_Event event;
 bool rightMouseButtonDown = false;
 int lastMouseX, lastMouseY;
 
-static std::array<array<glm::u8vec3, 256>, 256> texture;
-
-static void initializeTexture() {
-	for (int i = 0; i < texture.front().size(); i++)
-	{
-		for (int j = 0; j < texture.size(); j++)
-		{
-			const glm::u8vec3 whiteColor(255, 255, 255);
-			const glm::u8vec3 blackColor(0, 0, 0);
-			texture[i][j] = (i / 8 + j / 8) % 2 == 0 ? whiteColor : blackColor;
-		}
-	}
-
+// Inicialización de OpenGL
+void initOpenGL() {
+    glewInit();
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 }
 
+void configureCamera() {
+    glm::dmat4 projectionMatrix = glm::perspective(glm::radians(45.0), static_cast<double>(WINDOW_SIZE.x) / WINDOW_SIZE.y, 0.1, 100.0);
+    glm::dmat4 viewMatrix = camera.view();
 
+    glMatrixMode(GL_PROJECTION);
+    glLoadMatrixd(glm::value_ptr(projectionMatrix));
+    glMatrixMode(GL_MODELVIEW);
+    glLoadMatrixd(glm::value_ptr(viewMatrix));
+}
+std::string getFileExtension(const std::string& filePath) {
+    // Find the last dot in the file path
+    size_t dotPosition = filePath.rfind('.');
+
+    // If no dot is found, return an empty string
+    if (dotPosition == std::string::npos) {
+        return "";
+    }
+
+    // Extract and return the file extension
+    return filePath.substr(dotPosition + 1);
+}
+
+// Función para cargar archivos en GameObject
+void handleFileDrop(const std::string& filePath) {
+    auto extension = getFileExtension(filePath);
+    auto mesh = make_shared<Mesh>();
+    auto imageTexture = make_shared<Image>();
+
+    if (extension == "obj" || extension == "fbx" || extension == "dae") {
+        mesh->LoadFile(filePath.c_str());
+        go.setMesh(mesh);
+    }
+    else if (extension == "png" || extension == "jpg" || extension == "bmp") {
+        imageTexture->loadTexture(filePath);
+        go.setTextureImage(imageTexture);
+    }
+    else {
+        cerr << "Unsupported file extension: " << extension << endl;
+    }
+}
 static void drawFloorGrid(int size, double step) {
-	glColor3ub(0, 0, 0);
-	glBegin(GL_LINES);
-	for (double i = -size; i <= size; i += step) {
-		glVertex3d(i, 0, -size);
-		glVertex3d(i, 0, size);
-		glVertex3d(-size, 0, i);
-		glVertex3d(size, 0, i);
-	}
-	glEnd();
+    glColor3ub(0, 0, 0);
+    glBegin(GL_LINES);
+    for (double i = -size; i <= size; i += step) {
+        glVertex3d(i, 0, -size);
+        glVertex3d(i, 0, size);
+        glVertex3d(-size, 0, i);
+        glVertex3d(size, 0, i);
+    }
+    glEnd();
 }
 
-static void display_func() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+// Función de renderizado
+void display_func() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    configureCamera();
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadMatrixd(&camera.view()[0][0]);
+    // Llamar aquí a la función de dibujo de tu objeto
+    go.draw();
 
-	drawFloorGrid(16, 0.25);
-	//red_triangle.draw();
-	//green_triangle.draw();
-	//blue_triangle.draw();
-	//cube.draw();
-	loadedMesh.draw();
-
-	glutSwapBuffers();
+    // Otros elementos de la escena, como la cuadrícula, etc.
+    drawFloorGrid(16, 0.25);
 }
 
-static void init_opengl() {
-	glewInit();
-
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-	glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
-	glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
-
-	glEnable(GL_POLYGON_SMOOTH);
-	glEnable(GL_LINE_SMOOTH);
-	glEnable(GL_POINT_SMOOTH);
-
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
-
-	glEnable(GL_DEPTH_TEST);
-
-	glClearColor(0.5, 0.5, 0.5, 1.0);
+// Funciones de manejo de mouse
+void mouseButton_func(int button, int state, int x, int y) {
+    if (button == SDL_BUTTON_RIGHT) {
+        rightMouseButtonDown = (state == SDL_PRESSED);
+        lastMouseX = x;
+        lastMouseY = y;
+    }
 }
 
-static void reshape_func(int width, int height) {
-	glViewport(0, 0, width, height);
-	camera.aspect = static_cast<double>(width) / height;
-	glMatrixMode(GL_PROJECTION);
-	glLoadMatrixd(&camera.projection()[0][0]);
+void mouseMotion_func(int x, int y) {
+    if (rightMouseButtonDown) {
+        int deltaX = x - lastMouseX;
+        int deltaY = y - lastMouseY;
+
+        const double sensitivity = 0.1;
+        camera.transform().rotate(glm::radians(-deltaX * sensitivity), vec3(0, 1, 0));
+        camera.transform().rotate(glm::radians(-deltaY * sensitivity), vec3(1, 0, 0));
+
+        lastMouseX = x;
+        lastMouseY = y;
+    }
 }
 
-static void mouseWheel_func(int wheel, int direction, int x, int y) {
-	camera.transform().translate(vec3(0, 0, direction * 0.1));
-}
-static void mouse_func(int button, int state, int x, int y) {
-	if (button == GLUT_RIGHT_BUTTON) {
-		rightMouseButtonDown = (state == GLUT_DOWN);
-		lastMouseX = x;
-		lastMouseY = y;
-	}
-}
-static void motion_func(int x, int y) {
-	if (rightMouseButtonDown) {
-		int deltaX = x - lastMouseX;
-		int deltaY = y - lastMouseY;
+//static void idle_func() {
+//    /*const double move_speed = 0.1;
+//    int modifiers = glutGetModifiers();
+//
+//    if (rightMouseButtonDown) {
+//        if (keyStates['w'] || keyStates['W']) {
+//            camera.transform().translate(vec3(0, 0, move_speed));
+//        }
+//        if (keyStates['s'] || keyStates['S']) {
+//            camera.transform().translate(vec3(0, 0, -move_speed));
+//        }
+//        if (keyStates['a'] || keyStates['A']) {
+//            camera.transform().translate(vec3(move_speed, 0, 0));
+//        }
+//        if (keyStates['d'] || keyStates['D']) {
+//            camera.transform().translate(vec3(-move_speed, 0, 0));
+//        }
+//    }*/
+//}
 
-		
-		const double sensitivity = 0.1;
-		double angleX = deltaX * sensitivity;
-		double angleY = deltaY * sensitivity;
-
-		
-		camera.transform().rotate(glm::radians(-angleX), vec3(0, 1, 0));
-		camera.transform().rotate(glm::radians(-angleY), vec3(1, 0, 0));
-
-		lastMouseX = x;
-		lastMouseY = y;
-	}
-}
-void keyboardDown_func(unsigned char key, int x, int y) {
-	keyStates[key] = true;
-}
-
-void keyboardUp_func(unsigned char key, int x, int y) {
-	keyStates[key] = false;
-}
-//Similar to update function in SDL
-static void idle_func() {
-	const double move_speed = 0.1;
-	int modifiers = glutGetModifiers();
-
-	if (rightMouseButtonDown) {
-		if (keyStates['w'] || keyStates['W']) {
-			camera.transform().translate(vec3(0, 0, move_speed));
-		}
-		if (keyStates['s'] || keyStates['S']) {
-			camera.transform().translate(vec3(0, 0, -move_speed));
-		}
-		if (keyStates['a'] || keyStates['A']) {
-			camera.transform().translate(vec3(move_speed, 0, 0));
-		}
-		if (keyStates['d'] || keyStates['D']) {
-			camera.transform().translate(vec3(-move_speed, 0, 0));
-		}
-	}
-
-	// Animate triangles
-	glutPostRedisplay();
+void mouseWheel_func(int direction) {
+    camera.transform().translate(vec3(0, 0, direction * 0.1));
 }
 
 int main(int argc, char* argv[]) {
-	ilInit();
-	iluInit();
-	ilutInit();
-	// Iniit window and context
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(1280, 720);
-	glutCreateWindow("Glut Simple Example");
+    ilInit();
+    iluInit();
 
-	// Init OpenGL
-	init_opengl();
+    MyWindow window("SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
+    initOpenGL();
 
-	// Init camera
-	camera.transform().pos() = vec3(0, 1, 4);
-	camera.transform().rotate(glm::radians(180.0), vec3(0, 1, 0));
+    // Posición inicial de la cámara
+    camera.transform().pos() = vec3(0, 1, 4);
+    camera.transform().rotate(glm::radians(180.0), vec3(0, 1, 0));
 
+    while (window.processEvents() && window.isOpen()) {
+        const auto t0 = hrclock::now();
 
-	auto mesh = make_shared<Mesh>();
-	auto imageTexture = make_shared<Image>();
+        display_func();
+        //idle_func();
+        window.swapBuffers();
 
-	mesh->LoadFile("BakerHouse.fbx");
-	loadedMesh.setMesh(mesh);
-	imageTexture->loadTexture("Baker_house.png");
-	loadedMesh.setTextureImage(imageTexture);
+        const auto t1 = hrclock::now();
+        const auto dt = t1 - t0;
+        if (dt < FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
 
-	//loadedMesh.loadFile("BakerHouse.fbx");
-	////loadedMesh.CreateDefaultTexture();
-	//loadedMesh.loadTexture("Baker_house.png");
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_DROPFILE:
+                handleFileDrop(event.drop.file);
+                SDL_free(event.drop.file);
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONUP:
+                mouseButton_func(event.button.button, event.button.state, event.button.x, event.button.y);
+                break;
+            case SDL_MOUSEMOTION:
+                mouseMotion_func(event.motion.x, event.motion.y);
+                break;
+            case SDL_MOUSEWHEEL:
+                mouseWheel_func(event.wheel.y);
+                break;
+            }
+        }
+    }
 
-
-
-	// Set Glut callbacks
-	glutDisplayFunc(display_func);
-	glutIdleFunc(idle_func);
-	glutReshapeFunc(reshape_func);
-
-	//Camera movement
-	glutMouseWheelFunc(mouseWheel_func);
-	glutKeyboardFunc(keyboardDown_func);
-	glutKeyboardUpFunc(keyboardUp_func);
-	glutMouseFunc(mouse_func);
-	glutMotionFunc(motion_func);
-
-
-	// Enter glut main loop
-	glutMainLoop();
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
